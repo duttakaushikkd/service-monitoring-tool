64c9fe3663ef1718533f9af877c611578f2d23a9
duttakaushikkd <99733850+duttakaushikkd@users.noreply.github.com>
2025-12-20 15:38:42 +0530

    Service initial commits

---
+++ decision-service/src/test/java/com/example/decision/decision_service/DecisionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.decision.decision_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecisionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
