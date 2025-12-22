457770845cbc911aa674675edf89cd2670334538
duttakaushikkd <99733850+duttakaushikkd@users.noreply.github.com>
2025-12-22 22:02:36 +0530

    Monitoring Service config initial commit

---
+++ monitoring-service/src/main/java/com/example/decision/monitoring_service/config/KafkaConfig.java
@@ -0,0 +1,8 @@
+package com.example.decision.monitoring_service.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+public class KafkaConfig {
+}
