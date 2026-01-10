93d3f4681c11475bb3e06f112bbaf76832fbc5c2
duttakaushikkd <99733850+duttakaushikkd@users.noreply.github.com>
2026-01-10 16:29:19 +0530

    Issuer resolved

--- frontend/service-monitoring-frontend/src/App.jsx
+++ frontend/service-monitoring-frontend/src/App.jsx
@@ -1,20 +1,14 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import Monitor from './pages/Monitor'
 
 function App() {
   return (
     <>
       <div>
-        <BrowserRouter>
           <Navbar />
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/status" element={<Monitor />} />
           </Routes>
-        </BrowserRouter>
       </div>
 
     </>
