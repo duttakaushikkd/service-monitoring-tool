debe2e2f237c7f5c3ab1ca468f0045255b2d3142
duttakaushikkd <99733850+duttakaushikkd@users.noreply.github.com>
2026-01-10 16:32:29 +0530

    React router

--- frontend/service-monitoring-frontend/src/App.jsx
+++ frontend/service-monitoring-frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Monitor from './pages/Monitor'
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <div>
           <Navbar />
           <Routes>
+             <Route path="/" element={<Home />} />
+             <Route path="/monitoring" element={<Monitor />} />
           </Routes>
       </div>
 
